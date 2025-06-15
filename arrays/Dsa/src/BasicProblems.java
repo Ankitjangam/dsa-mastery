26d176a6a6658323505d2acae2247bd6429bde21
Ankit Jangam <jangamankit17@gmail.com>
2025-06-15 11:47:44 +0530

    Basic Problems

---
+++ arrays/Dsa/src/BasicProblems.java
@@ -0,0 +1,17 @@
+public class BasicProblems {
+    public static void findMAx(int[] arr) {
+
+
+    }
+
+
+    public static void findMin(int[]arr) {
+
+
+    }
+
+
+    public static void sumOfAll(int[] arr) {
+
+    }
+}
